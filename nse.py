187b284022e3a6b30536cf3778f1f4ad223ad999
kwoshvick <kwoshvick@gmail.com>
2017-05-28 13:53:36 +0300

    added errorlogging when url is not found

--- nse.py
+++ nse.py
@@ -1,11 +1,40 @@
 from src import *
+import calendar
 
 crawl = Crawler()
+cal = Calender()
 
 # date = https://live.mystocks.co.ke/price_list/20160501
-crawl.getURLData(20160501)
+
+year = 2006
+finalYear = 2006
+totalMonths = 12
+startMonth = 9
+
+while year < (finalYear+1):
+    while startMonth < 13:
+        y = cal.getDayInMonth(year, startMonth)
+        for i in y:
+            crawl.getURLData(int(i))
+        startMonth += 1
+    startMonth = 1
+    year += 1
 
 
-# cal = Calender()
-# y = cal.getDayInMonth(2017,5)
-# print(y)
+
+
+
+
+
+
+
+
+
+# c = calendar.calendar(2006)
+#
+# print(c)
+
+# y = cal.getDayInMonth(2006,2)
+# for i in y:
+#     crawl.getURLData(int(i))
+#
--- src/crawler_.py
+++ src/crawler_.py
@@ -1,5 +1,8 @@
 import os
 import csv
+import logging
+import datetime
+import urllib.error as error
 import urllib.request as rq
 from bs4 import BeautifulSoup
 
@@ -10,63 +13,73 @@ class Crawler:
         shareDetails list contains all the details of a particular company .i.e name, code , volume e.t.c
         shareElement list makes up the Share Details which makes up the dailyShares list
         '''
-        self.dailyShares = list()
+        self.dailyShares2 = list()
+        logging.basicConfig(filename='errorlog/error.log', level=logging.ERROR)
+
 
     def getURLData(self,date):
+        self.dailyShares = list()
         self.date = str(date)
-        self.createFolder('data/year/'+self.date[0:4])
+        self.createFolder('data/daily/'+self.date[0:4])
         url = "https://live.mystocks.co.ke/price_list/" + self.date
         try:
             html = rq.urlopen(url).read()
-        except ValueError:
-            print("no date")
-        finally:
             self.soup = BeautifulSoup(html, "lxml")
             self.extractURLData()
+        except error.HTTPError as e:
+            now = datetime.datetime.now()
+            errorMessage = str(now)+' - '+str(e)+' Date: '+str(self.date)
+            print('Please check error log file in errorlog directory')
+            logging.error(errorMessage)
+
 
     def extractURLData(self):
-        table = self.soup.find("table", {"class": "tblHoverHi"})
-        for row in table.findAll("tr"):
-            shareDetails = list()
-            shareElements = list()
-            for element in row.findAll("td"):
-                # the share categories i.e banking, manufacturing
-                for heading in element.findAll("h3"):
-                    _heading = heading.string
-                # the share company name
-                for item in element.findAll("a"):
-                    shareElements.append(item.string)
-                # the shares details i.e price,volume
-                if element.string != heading.string:
-                    shareElements.append(element.string)
-            # removes empty and None arrays, clean up from html extracted data
-            if (shareElements != [] and len(shareElements) > 1):
-                # code
-                shareDetails.append(shareElements[0])
-                # name
-                shareDetails.append(shareElements[1])
-                # lowest price
-                if shareElements[5] == None:
-                    shareDetails.append(shareElements[5])
-                else:
-                    shareDetails.append(float(shareElements[5].replace(',', '')))
-                # highest price
-                if shareElements[6] == None:
-                    shareDetails.append(shareElements[6])
-                else:
-                    shareDetails.append(float(shareElements[6].replace(',', '')))
-                # price
-                if shareElements[7] == None:
-                    shareDetails.append(shareElements[7])
-                else:
-                    shareDetails.append(float(shareElements[7].replace(',', '')))
-                # volume
-                if (shareElements[12] == None) or (shareElements[12] == '-'):
-                    shareDetails.append(shareElements[12])
-                else:
-                    shareDetails.append(str(shareElements[12].replace(',', '')))
-                self.dailyShares.append(shareDetails)
-        self.saveCSV()
+        try:
+            table = self.soup.find("table", {"class": "tblHoverHi"})
+            for row in table.findAll("tr"):
+                shareDetails = list()
+                shareElements = list()
+                for element in row.findAll("td"):
+                    # the share categories i.e banking, manufacturing
+                    for heading in element.findAll("h3"):
+                        _heading = heading.string
+                    # the share company name
+                    for item in element.findAll("a"):
+                        shareElements.append(item.string)
+                    # the shares details i.e price,volume
+                    if element.string != heading.string:
+                        shareElements.append(element.string)
+                # removes empty and None arrays, clean up from html extracted data
+                if (shareElements != [] and len(shareElements) > 1):
+                    # code
+                    shareDetails.append(shareElements[0])
+                    # name
+                    shareDetails.append(shareElements[1])
+                    # lowest price
+                    if shareElements[5] == None:
+                        shareDetails.append(shareElements[5])
+                    else:
+                        shareDetails.append(float(shareElements[5].replace(',', '')))
+                    # highest price
+                    if shareElements[6] == None:
+                        shareDetails.append(shareElements[6])
+                    else:
+                        shareDetails.append(float(shareElements[6].replace(',', '')))
+                    # price
+                    if shareElements[7] == None:
+                        shareDetails.append(shareElements[7])
+                    else:
+                        shareDetails.append(float(shareElements[7].replace(',', '')))
+                    # volume
+                    if (shareElements[12] == None) or (shareElements[12] == '-'):
+                        shareDetails.append(shareElements[12])
+                    else:
+                        shareDetails.append(str(shareElements[12].replace(',', '')))
+                    self.dailyShares.append(shareDetails)
+            self.saveCSV()
+        except ValueError:
+            print("no date")
+
 
     def createFolder(self,path):
         folder = os.path.isdir(path)
@@ -76,7 +89,7 @@ class Crawler:
     def saveCSV(self):
         year = self.date[0:4]
         month = self.date[4:6]
-        path = 'data/year/'+str(year)+'/'+month+'/'
+        path = 'data/daily/'+str(year)+'/'+month+'/'
         folder = os.path.isdir(path)
         if folder == False:
             os.mkdir(path)
