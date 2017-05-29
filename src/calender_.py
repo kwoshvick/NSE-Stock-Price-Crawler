efb57b738f71bf491487058310f1f979592f473c
kwoshvick <kwoshvick@gmail.com>
2017-05-29 10:18:33 +0300

    crawled test data & removed holidays from list(static holidays)

--- src/calender_.py
+++ src/calender_.py
@@ -10,6 +10,7 @@ class Calender:
     def getDayInMonth(self,year,month):
         weekdays = list()
         self.month = month
+        print(self.month)
         self.year = year
         for date in self.calendar.itermonthdates(year,month):
             newDate = datetime.datetime.strptime(str(date), '%Y-%m-%d').strftime('%Y%m%d')
@@ -17,7 +18,9 @@ class Calender:
             if weekday:
                 if int(newDate[4:6]) == month:
                     weekdays.append(newDate)
-        return weekdays
+
+        tradingDays = self.holiday(weekdays)
+        return tradingDays
 
     # removes the weekends from the days of the month
     def isWeekday(self,date):
@@ -29,33 +32,36 @@ class Calender:
 
     # removes the holidays from the days of the month
     def holiday(self,days):
+        print(days)
         # New Year's Day 1st January
         if self.month == 1:
             day = str(self.year)+str(self.month)+'01'
             if day in days: days.remove(day)
         # Labour Day 1st May
-        elif(month == 5):
+        elif(self.month == 5):
             day = str(self.year) + str(self.month) + '01'
             if day in days: days.remove(day)
         # Madaraka Day* 1st June
-        elif(month == 6):
+        elif(self.month == 6):
             day = str(self.year) + str(self.month) + '01'
             if day in days: days.remove(day)
         # Mashujaa Day* 20th October
-        elif (month == 10):
+        elif (self.month == 10):
             day = str(self.year) + str(self.month) + '20'
             if day in days: days.remove(day)
         # Jamhuri (Independence) Day* 12th December
-        elif (month == 5):
+        elif (self.month == 12):
             day = str(self.year) + str(self.month) + '12'
             if day in days: days.remove(day)
-        # Christmas Day 25th December
-        elif (month == 5):
-            day = str(self.year) + str(self.month) + '25'
-            if day in days: days.remove(day)
-        # Boxing Day 26th December
-        elif (month == 5):
-            month.pop()
+            # Christmas Day 25th December
+            day2 = str(self.year) + str(self.month) + '25'
+            if day2 in days: days.remove(day2)
+            # Boxing Day 26th December
+            day3 = str(self.year) + str(self.month) + '26'
+            if day3 in days: days.remove(day3)
+        return days
+
+
 
 
         pass
