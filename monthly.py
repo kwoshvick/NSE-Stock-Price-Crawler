0f54925137de16fd4b1f6f93bdb4c9e8f3057629
kwoshvick <kwoshvick@gmail.com>
2017-06-05 17:00:38 +0300

    formated list format and created code name csv

--- monthly.py
+++ monthly.py
@@ -1,6 +1,18 @@
+import os
+
 import csv
-with open('data/monthly/12/20061201.csv', newline='') as csvfile:
+dailyPath = 'data/monthly/12/20061201.csv'
+monthlyPath = 'data/monthly/'
+fileName = '20061201.csv'
+
+
+with open(dailyPath, newline='') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
      for row in spamreader:
-         print(row)
-         # print(' '.join(row))
+         date = fileName.strip('.csv')
+         # append date to be the first element
+         row.insert(0, date)
+         file = open( monthlyPath + str(row[1]) + '.csv', 'a')
+         writeFile = csv.writer(file, delimiter=',')
+         writeFile.writerows([row])
+         file.close()
