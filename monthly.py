b9e322e7f6b1ae99dde1986d4d58c53c0c5ec25d
kwoshvick <kwoshvick@gmail.com>
2017-06-05 11:39:47 +0300

    converted csv data to list

--- monthly.py
+++ monthly.py
@@ -1,5 +1,6 @@
 import csv
 with open('data/monthly/12/20061201.csv', newline='') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
      for row in spamreader:
-         print(' '.join(row))
+         print(row)
+         # print(' '.join(row))
