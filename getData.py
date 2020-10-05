6b2393c6f85c38a4c59a0a267008aada2d72c041
kwoshvick <kwoshvick@gmail.com>
2020-10-05 22:43:17 +0300

    September 2020

--- getData.py
+++ getData.py
@@ -9,11 +9,11 @@ formatter = FormatData()
 # start year
 startYear = 2020
 # start month
-startMonth = 5
+startMonth = 9
 # end year
 endYear = 2020
 # end month
-endMonth = 8
+endMonth = 9
 
 print("Preparing Daily Data")
 while startYear < (endYear+1):
